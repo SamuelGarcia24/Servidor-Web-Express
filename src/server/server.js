7bccb87314729de4219d4b0d495eb48317ca71ab
Samuel <samuelemilio26@gmail.com>
2024-04-20 16:48:34 -0500

    creamos esta nueva rama para ver la diferencia entre el viejo y la forma moderna

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const path = require('path') 
+import express from 'express'
+import path from 'path'
 
-const startServer = (options) => {
+export const startServer = (options) => {
     const { port, public_path = 'public' } = options
     
     const app = express()
@@ -21,8 +21,4 @@ const startServer = (options) => {
     })
 
 
-}
-
-module.exports = {
-    startServer
 }
