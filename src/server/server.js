f4a8da8025579436a2dbfe6aac37033c49be18f9
Samuel <samuelemilio26@gmail.com>
2024-04-20 16:29:27 -0500

    servidor web con express js

---
+++ src/server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path') 
+
+const startServer = (options) => {
+    const { port, public_path = 'public' } = options
+    
+    const app = express()
+
+    //para poder usa middlewares use la palabra use (express)
+    app.use(express.static(public_path)) //ponemos disponible el contenido estatico
+
+    app.get('/pagina', (req, res)=>{
+        const indexPath = path.join(__dirname + `../../../${public_path}/index.html`)
+        res.sendFile(indexPath)
+    })
+
+    //lo usamos para incializar el servidor, le damos el puerto
+
+    app.listen(port, () => {
+        console.log(`Escuchando el puerto ${port}`)
+    })
+
+
+}
+
+module.exports = {
+    startServer
+}
